c960a5fdeace27b5794b791e8cbcdb20d433145a
Mihail-Makei <mihasiknet@phystech.edu>
2023-08-20 19:37:07 +0200

    Double comparator added

--- solver.cpp
+++ solver.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 
+const double EPS = 1e-9;
+bool cmp_doubles(double a, double b) {
+  return fabs(a - b) < EPS;
+}
+
 int main() {
   float a = 0, b = 0, c = 0;
   int res = scanf("%f", &a);
@@ -18,9 +23,9 @@ int main() {
     printf("Enter FLOATS next time, you a faggot!\n");
     return 0;
   }
-  if (a == 0) {
-    if (b == 0) {
-      if (c == 0) {
+  if (cmp_doubles(a, 0)) {
+    if (cmp_doubles(b, 0)) {
+      if (cmp_doubles(c, 0)) {
         printf("Infinity roots\n");
       } else {
         printf("No roots\n");
